d8c63e7a536b0f27240d1979b5cd97202fdad98f
Kirill <kurdyukov_kirya@mail.ru>
2020-07-06 07:42:23 +0300

    added condition

--- labs с/lab 4 BigInteger.cpp
+++ labs с/lab 4 BigInteger.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
 
 // immutable class
 class BigInteger {
@@ -230,7 +234,7 @@ public:
         }
         BigInteger ans = {false, {0}};
         while (temp.comparator(number) >= 0) {
-            bool flag = false;
+            bool iterationDone = false;
             for (int i = temp.getNumber().size() - 1; i >= 0; i--) {
                 std::vector<int> ranks;
                 for (int j = i; j < temp.getNumber().size(); j++) {
@@ -239,7 +243,7 @@ public:
                 for (int k = 9; k > 0; k--) {
                     cnt = BigInteger(false, {k}).multiply(number);
                     if (BigInteger(false, ranks).comparator(cnt) >= 0) {
-                        flag = true;
+                        iterationDone = true;
                         ranks = BigInteger(false, {k}).multiply(number).getNumber();
                         reverse(ranks.begin(), ranks.end());
                         std::vector<int> newObject;
@@ -254,10 +258,10 @@ public:
                         BigInteger cur = {false, ranks};
                         temp = temp.subtract(cur);
                     }
-                    if (flag)
+                    if (iterationDone)
                         break;
                 }
-                if (flag)
+                if (iterationDone)
                     break;
             }
         }
